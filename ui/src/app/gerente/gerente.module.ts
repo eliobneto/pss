6ac606bebb17c2e833992539972a38be97b3858c
CharlesChaclim <chaclimesteves@gmail.com>
2018-04-28 13:44:16 -0300

    Correção de Erros

--- ui/src/app/gerente/gerente.module.ts
+++ ui/src/app/gerente/gerente.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateComponent } from './create/create.component';
+import {CpfCnpjModule} from "ng2-cpf-cnpj";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, CpfCnpjModule,FormsModule
   ],
   declarations: [CreateComponent],
   exports: [
