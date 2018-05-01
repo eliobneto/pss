0160507fd2fb09fc40fcdbee13aa3bcccae2a02d
leonardo <ra98995@uem.br>
2018-05-01 14:16:01 -0300

    fix error

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-
 import {AppComponent} from './app.component';
-import {CategoriasModule} from "./categorias/categorias.module";
+import {CategoriasModule} from './categorias/categorias.module';
 
 
 @NgModule({
