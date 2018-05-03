26c46ac53113f7d42c5492667e22032843fbfdd6
leonardo <ra98995@uem.br>
2018-05-03 20:34:52 -0300

    the previous commit was fail, go back to back implementation of API

--- api/src/main/java/com/lelo452/boxitapiv2/resources/exceptions/ResourceExceptionHandler.java
+++ api/src/main/java/com/lelo452/boxitapiv2/resources/exceptions/ResourceExceptionHandler.java
@@ -28,7 +28,7 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getCause().toString(), request.getRequestURI());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
