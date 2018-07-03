34e88d7aaecb6e531a9b54580de3b2fcb2b9a742
lelo451 <ra98995@uem.br>
2018-07-02 22:27:02 -0300

    finish categorias module

--- ui/src/app/categoria/categoria-list/categoria-list.component.ts
+++ ui/src/app/categoria/categoria-list/categoria-list.component.ts
@@ -15,14 +15,70 @@ export class CategoriaListComponent implements OnInit {
 
   cols: any[];
 
+  selectedCat: Categoria;
+
+  newCat: boolean;
+
+  catego = new Categoria();
+
+  displayDialog: boolean;
+
   ngOnInit() {
-    this.categoria.get()
-      .then(
-        c => this.cat = c
-      );
+    this.populate();
 
     this.cols = [
       { field: 'name', header: 'Nome'}
     ];
+  }
+
+  populate() {
+    this.categoria.get()
+      .then(
+        c => this.cat = c
+      );
+  }
+
+  showAdd() {
+    this.newCat = true;
+    this.catego = new Categoria();
+    this.displayDialog = true;
+  }
+
+  save() {
+    const cats = [...this.cat];
+    if (this.newCat) {
+      cats.push(this.catego);
+    } else {
+      cats[cats.indexOf(this.selectedCat)] = this.catego;
+    }
+
+    this.cat = cats;
+    this.catego = null;
+    this.displayDialog = false;
+  }
+
+  delete() {
+    if (confirm('Essa ação não poderá ser desfeita')) {
+      const index = this.cat.indexOf(this.selectedCat);
+      this.cat = this.cat.filter((val, i) => i !== index);
+      this.catego = null;
+      this.displayDialog = false;
+    } else {
+      this.displayDialog = false;
+    }
+  }
+
+  onRowSelect(event) {
+    this.newCat = false;
+    this.catego = this.cloneCat(event.data);
+    this.displayDialog = true;
+  }
+
+  cloneCat(c: Categoria): Categoria {
+    let cate = new Categoria();
+    for (const prop in c) {
+      cate[prop] = c[prop];
+    }
+    return cate;
   }
 }
